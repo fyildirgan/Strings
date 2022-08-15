cf66aff7e0203f09e71b4454a6da233358c39d12
Hasan Fatih Yıldırgan <fatihyildirgan@hotmail.com>
2022-08-15 21:04:52 +0300

    new

---
+++ strings.py
@@ -0,0 +1,29 @@
+# Karakter Dizileri (Strings)
+# Karakter formunda olan verileri tutmak için kullanılan veri yapısıdır.
+
+print("SalvadorDali")
+print('SalvadorDali')
+
+name = "SalvadorDali"
+print(name)
+# Çok satırlı Karakter Dizileri. 3 tırnak ile kullanılmaktadır.
+long_str = """Veri Yapıları: Hızlı Özet,
+Sayılar(Numbers): int, float, complex,
+Karakter Dizileri(Strings): str,
+List, Dictionary, Tuple, Set,
+Boolean(True-False): bool"""
+print(long_str)
+# Karakter Dizierinin Elemanlarına Erişmek
+name1 = "Coconat"
+print(name1[3])
+# Karakter Dizilerinde Slice İşlemi
+name2 = "Picasso"
+print(name2[0:2])
+# String İçerisinde Karakter Sorgulama
+print('Veri' in long_str)
+print('bool' in long_str)
+
+
+
+
+
